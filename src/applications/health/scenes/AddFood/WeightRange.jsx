a1980a52914d73f6ac8c9e6448a11d7c1ef28bd0
VladimirLysenkoIT <vladimirl.webmaster@gmail.com>
2020-05-07 07:37:18 +0200

    weight range

--- src/applications/health/scenes/AddFood/WeightRange.jsx
+++ src/applications/health/scenes/AddFood/WeightRange.jsx
@@ -1,24 +1,44 @@
 import React from "react";
 
 export const WeightRange = () => {
-    const [weight, setWeight] = React.useState(100);
-   const rangeOnchange = (e) => {
+  const [weight, setWeight] = React.useState(100);
+  const rangeOnchange = (e) => {
     const newWeight = e.target.value;
     setWeight(newWeight)
   }
+  let isButtonDown = false;
+  let incrementWeightInterwal;
+
+  const addWeight = () => {
+    const newWeight = (weight * 1 ) + 1;
+    setWeight(newWeight)
+  }
+
+  const minusWeight = () => {
+    const newWeight = (weight * 1 ) - 1;
+    setWeight(newWeight)
+  }
+
+  const onMouseUpHandler = () => {
+    clearInterval(incrementWeightInterwal)
+  }
+
+  const onMouseDownHandler = () => {
+    console.log(1);
+    setTimeout(function(){ 
+      alert(12);
+      incrementWeightInterwal = setInterval(function(){ console.log(1) }, 2000);
+    }, 1000);
+  }
+
+  
 
   return (
     <div className="weightRange">
         <form action="">
-            <p class="range-field">
-            <div className='numericInputBox'>
-                    <div className='labelBox'><label htmlFor=''>Poids (gr)</label></div>
-                    <div className='inputBox'>
-                        <input type='text' name='fat' value={weight} onChange={rangeOnchange} />
-                    </div>
-            </div>
-            
-            </p>
+          <button onClick={minusWeight} onMouseUp={onMouseUpHandler} onMouseDown={onMouseDownHandler} className="back" type="button"><i className="material-icons">navigate_before</i></button>
+            <input type='text' name='fat' value={weight} onChange={rangeOnchange} />
+          <button onClick={addWeight} onMouseUp={onMouseUpHandler} onMouseDown={onMouseDownHandler} className={`forward `} type="button"><i className="material-icons">navigate_next</i></button>
         </form>
     </div>
   );
