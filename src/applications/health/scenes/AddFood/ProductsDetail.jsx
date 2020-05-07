fa5d9cc7a894d597117f6c0b7fa1d155a34e594d
Vladimir Lysenko <VladimirLysenko@mediaspole.local>
2020-05-07 16:16:54 +0200

    I done weight ranger/switcher. And i ajusted productDetail page

--- src/applications/health/scenes/AddFood/ProductsDetail.jsx
+++ src/applications/health/scenes/AddFood/ProductsDetail.jsx
@@ -3,44 +3,122 @@ import M from 'materialize-css';
 import { WeightRange } from "./WeightRange";
 
 export const ProductsDetail = ({ currentProduct }) => {
+  const [weightCoefficient, setWeightCoefficient] = React.useState(1);
+
   return (
     <div className="productDetail">
       {currentProduct ? (
         <div className="details">
           <div className="header">
             <h3>{`${currentProduct.name}`}</h3>
-            <WeightRange />
+            <WeightRange setWeightCoefficient={setWeightCoefficient} />
           </div>
-          
-          <p>{`Type: ${currentProduct.type}`}</p>
-          <p>{`Calories: ${currentProduct.nutritionFacts.calories}`}</p>
-          <p>{`Lipides totales: ${
-            currentProduct.nutritionFacts.lipides.total
-          }`}</p>
-          <p>Dont:</p>
-          <p>{`Saturés: ${currentProduct.nutritionFacts.lipides.saturated}`}</p>
-          <p>{`Poly-insaturés: ${
-            currentProduct.nutritionFacts.lipides.monounsaturated
-          }`}</p>
-          <p>{`Mono-insaturés: ${
-            currentProduct.nutritionFacts.lipides.polyunsaturated
-          }`}</p>
-          <p>{`Cholestérol: ${currentProduct.nutritionFacts.cholesterol}`}</p>
-          <p>{`Sodium: ${currentProduct.nutritionFacts.sodium}`}</p>
-          <p>{`Potassium: ${currentProduct.nutritionFacts.potassium}`}</p>
-          <p>{`Glucides: ${currentProduct.nutritionFacts.carbs.total}`}</p>
-          <p>Dont:</p>
-          <p>{`Fibres alimentaires: ${
-            currentProduct.nutritionFacts.carbs.fibres
-          }`}</p>
-          <p>{`Sucres: ${currentProduct.nutritionFacts.carbs.sucres}`}</p>
-          <p>{`Protéines: ${currentProduct.nutritionFacts.proteines}`}</p>
-          <p>{`Vitamin A: ${currentProduct.nutritionFacts.vitamin_A}`}</p>
-          <p>{`Calcium: ${currentProduct.nutritionFacts.calcium}`}</p>
-          <p>{`Vitamin C: ${currentProduct.nutritionFacts.vitamin_C}`}</p>
-          <p>{`Fet: ${currentProduct.nutritionFacts.fer}`}</p>
-          <p>{`Vitamin B6: ${currentProduct.nutritionFacts.vitamin_B6}`}</p>
-          <p>{`Magnesium: ${currentProduct.nutritionFacts.magnesium}`}</p>
+          <div className="productProperties">
+            <p>{`Type: ${currentProduct.type}`}</p>
+            <div className="part caloriePart">
+              <div className="partTitle">
+                <span className="title">Calories:</span>
+                <span className="amount">{(currentProduct.nutritionFacts.calories * weightCoefficient).toFixed(1)} </span>
+              </div>
+            </div>
+
+            <div className="part proteinPart">
+              <div className="partTitle">
+                <span className="title">Protéines:</span>
+                <span className="amount">{(currentProduct.nutritionFacts.proteines * weightCoefficient).toFixed(1)}</span>
+              </div>
+            </div>
+
+            <div className="part carbPart">
+              <div className="partTitle">
+                <span className="title">Glucides totales:</span>
+                <span className="amount">{(currentProduct.nutritionFacts.carbs.total * weightCoefficient).toFixed(1)}</span>
+              </div>
+               <div className="subDetails">
+                <span>Dont:</span>
+                <div>
+                  <span className="title">Fibres alimentaires: </span>
+                  <span className="amount">{(currentProduct.nutritionFacts.carbs.fibres * weightCoefficient).toFixed(1)}</span>
+                </div>
+                <div>
+                  <span className="title">Sucres: </span>
+                  <span className="amount">{(currentProduct.nutritionFacts.carbs.sucres * weightCoefficient).toFixed(1)}</span>
+                </div>  
+              </div>
+            </div>
+
+            <div className="part fatPart">
+              <div className="partTitle">
+                <span className="title">Lipides totales:</span>
+                <span className="amount">{(currentProduct.nutritionFacts.lipides.total * weightCoefficient).toFixed(1)}</span>
+              </div>
+               <div className="subDetails">
+                <span>Dont:</span>
+                <div>
+                  <span className="title">Saturés: </span>
+                  <span className="amount">{(currentProduct.nutritionFacts.lipides.saturated * weightCoefficient).toFixed(1)}</span>
+                </div>
+                <div>
+                  <span className="title">Poly-insaturés: </span>
+                  <span className="amount">{(currentProduct.nutritionFacts.lipides.monounsaturated * weightCoefficient).toFixed(1)}</span>
+                </div>  
+                <div>
+                  <span className="title">Mono-insaturés: </span>
+                  <span className="amount">{(currentProduct.nutritionFacts.lipides.polyunsaturated * weightCoefficient).toFixed(1)}</span>
+                </div>  
+                <div>
+                  <span className="title">Cholestérol: </span>
+                  <span className="amount">{(currentProduct.nutritionFacts.cholesterol * weightCoefficient).toFixed(1)}</span>
+                </div>
+              </div>
+            </div>
+
+            
+
+            <div className="part mineralsPart">
+              <div className="partTitle">
+                <span className="title">Vitamins & Minerals:</span>
+              </div>
+               <div className="subDetails">
+                <div>
+                  <span className="title">Sodium: </span>
+                  <span className="amount">{(currentProduct.nutritionFacts.sodium * weightCoefficient).toFixed(1)}</span>
+                </div>
+                <div>
+                  <span className="title">Potassium: </span>
+                  <span className="amount">{(currentProduct.nutritionFacts.potassium * weightCoefficient).toFixed(1)}</span>
+                </div>
+                <div>
+                  <span className="title">Magnesium: </span>
+                  <span className="amount">{(currentProduct.nutritionFacts.magnesium * weightCoefficient).toFixed(1)}</span>
+                </div>
+                <div>
+                  <span className="title">Calcium: </span>
+                  <span className="amount">{(currentProduct.nutritionFacts.calcium * weightCoefficient).toFixed(1)}</span>
+                </div>
+                <div>
+                  <span className="title">Fer: </span>
+                  <span className="amount">{(currentProduct.nutritionFacts.fer * weightCoefficient).toFixed(1)}</span>
+                </div>
+                <div>
+                  <span className="title">Vitamin A: </span>
+                  <span className="amount">{(currentProduct.nutritionFacts.vitamin_A * weightCoefficient).toFixed(1)}</span>
+                </div>
+                <div>
+                  <span className="title">Vitamin C: </span>
+                  <span className="amount">{(currentProduct.nutritionFacts.vitamin_C * weightCoefficient).toFixed(1)}</span>
+                </div>
+                <div>
+                  <span className="title">Vitamin B6: </span>
+                  <span className="amount">{(currentProduct.nutritionFacts.vitamin_B6 * weightCoefficient).toFixed(1)}</span>
+                </div>
+              </div>
+            </div>
+           
+           
+
+           
+          </div>
         </div>
       ) : (
         <div className="noDetails">
--- src/applications/health/scenes/AddFood/WeightRange.jsx
+++ src/applications/health/scenes/AddFood/WeightRange.jsx
@@ -1,24 +1,61 @@
 import React from "react";
 
-export const WeightRange = () => {
-    const [weight, setWeight] = React.useState(100);
-   const rangeOnchange = (e) => {
+export const WeightRange = ({ setWeightCoefficient }) => {
+  const [weight, setWeight] = React.useState(100);
+  const [intervalId, setIntervalId] = React.useState(false);
+  const interval = 100;
+  let longPressNewWeight = weight;
+
+  React.useEffect(()=>{
+    const weightCoefficient = weight / 100;
+    setWeightCoefficient(weightCoefficient)
+
+    longPressNewWeight = weight
+    }, [weight]
+  )
+
+  const textFieldOnChangeHandler = (e) => {
     const newWeight = e.target.value;
+    setWeight(newWeight);
+  }
+  
+  const addWeight = () => {
+    let newWeight = weight + 1;
+    
     setWeight(newWeight)
+  }
+
+  const minusWeight = () => {
+    let newWeight = weight*1 - 1;
+    setWeight(newWeight)
+  }
+
+  const longAddPressHandler = () => {
+    setIntervalId(setInterval(() => {
+      longPressNewWeight += 1;
+    setWeight(longPressNewWeight)
+
+      }, interval))
+  }
+
+  const longMinusPressHandler = () => {
+    setIntervalId(setInterval(() => {
+      longPressNewWeight -= 1;
+    setWeight(longPressNewWeight)
+
+      }, interval))
+  }
+
+  const buttonUpHandler = () => {
+    clearInterval(intervalId);
   }
 
   return (
     <div className="weightRange">
         <form action="">
-            <p class="range-field">
-            <div className='numericInputBox'>
-                    <div className='labelBox'><label htmlFor=''>Poids (gr)</label></div>
-                    <div className='inputBox'>
-                        <input type='text' name='fat' value={weight} onChange={rangeOnchange} />
-                    </div>
-            </div>
-            
-            </p>
+          <button onClick={minusWeight} onMouseDown={longMinusPressHandler} onMouseUp={buttonUpHandler} className="back" type="button"><i className="material-icons">navigate_before</i></button>
+            <input type='text' name='fat' value={weight + 'gr'} onChange={textFieldOnChangeHandler} />
+          <button onClick={addWeight} onMouseDown={longAddPressHandler} onMouseUp={buttonUpHandler} className={`forward`} type="button"><i className="material-icons">navigate_next</i></button>
         </form>
     </div>
   );
